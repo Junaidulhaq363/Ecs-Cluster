13ca2dc3c13338188cf03c851e0aaa5a0cb69b73
Junaidul Haq <haqjunaidul5@gmail.com>
2022-11-21 11:51:41 +0530

    working_ECS-Cluster

---
+++ Provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "account1"
+
+  default_tags {
+    tags = {
+      Name = "architect-demo"
+    }
+  }
+}
